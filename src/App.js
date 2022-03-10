0d8a802fb0d55529fafa13ee0043122b833e5193
PavanSpacecode <pavan.potdar@spacecode.com>
2022-03-10 22:52:18 +0530

    navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import LoginPage from './components/login/LoginPage';
 import Main from './main/Main';
@@ -7,15 +7,7 @@ import Main from './main/Main';
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
-      <BrowserRouter>
-          {/* <Main/> */}
-          <switch>
-          <Route exact path="/"  >
-            <LoginPage/>
-        </Route>
-        </switch>
-       </BrowserRouter>
+          <Main/>
     </div>
   );
 }
--- src/main/Main.js
+++ src/main/Main.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import LoginPage from '../components/login/LoginPage'
+import ClientPage from '../components/client/ClientPage'
+import DashboardPage from '../components/dashboard/DashboardPage'
 
 function Main() {
   return (
     <div>
-      <div>
-        <Route exact path="/"  >
-            <LoginPage/>
-        </Route>
-        </div>
-        
+        <Routes>
+          <Route exact path='/' element={<LoginPage />} />
+          <Route exact path='/client' element={<ClientPage />} />
+          <Route exact path='/dashbord' element={<DashboardPage />} />
+        </Routes>
     </div>
   )
 }
