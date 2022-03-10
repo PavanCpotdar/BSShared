0cab4f6a11814bc82814a76b0b153f8b46cb1632
vikaslathiya <lathiyavikas@gmail.com>
2022-03-10 19:55:50 +0530

    change file structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-
-import { BrowserRouter, Route } from 'react-router-dom';
+import React, {Fragment} from "react";
+import {Route, Switch} from 'react-router-dom';
 import './App.css';
-import LoginPage from './components/login/LoginPage';
-import Main from './main/Main';
+import LoginPage from './components/login';
+
 
 function App() {
-  return (
-    <div className="App">
-      <h1>hello</h1>
-      <BrowserRouter>
-          {/* <Main/> */}
-          <switch>
-          <Route exact path="/"  >
-            <LoginPage/>
-        </Route>
-        </switch>
-       </BrowserRouter>
-    </div>
-  );
+    return (
+        <Fragment>
+            <Switch>
+                <Route exact path="/">
+                    <LoginPage/>
+                </Route>
+            </Switch>
+        </Fragment>
+    );
 }
 
 export default App;
