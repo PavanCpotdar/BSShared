3fcbc0022c5ca3d0fb84b8f4ae5f135fc9de4d7b
PavanSpacecode <pavan.potdar@spacecode.com>
2022-03-10 19:21:45 +0530

    secound

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Main from './main/Main';
 function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
+      <h1>hello</h1>
       <BrowserRouter>
           {/* <Main/> */}
           <switch>
