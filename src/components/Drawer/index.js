27671bb2d2b81de8769ff84b58be528197a4437e
vikaslathiya <lathiyavikas@gmail.com>
2022-03-11 10:07:53 +0530

    change file structure

---
+++ src/components/Drawer/index.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from "react";
+
+const Drawer = () => {
+    return (
+        <Fragment>
+            <h2>Make Drawer here</h2>
+        </Fragment>
+    );
+};
+
+export default Drawer;
